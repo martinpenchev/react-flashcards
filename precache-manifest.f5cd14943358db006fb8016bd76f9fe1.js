55f854fddbb2f71dd873233c622ad25c2bc1ce38
Martin Penchev <martin.s.penchev@gmail.com>
2021-02-21 12:05:33 +0100

    Updates

---
+++ precache-manifest.f5cd14943358db006fb8016bd76f9fe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adbe9fdc88480637266e",
+    "url": "/react-flashcards/static/css/main.2ca1cb36.chunk.css"
+  },
+  {
+    "revision": "adbe9fdc88480637266e",
+    "url": "/react-flashcards/static/js/main.80c5e5bd.chunk.js"
+  },
+  {
+    "revision": "f21f1dcf700c93663418",
+    "url": "/react-flashcards/static/js/runtime~main.65a1b0a5.js"
+  },
+  {
+    "revision": "f399d68a0e6d0c84be0d",
+    "url": "/react-flashcards/static/css/2.527206e4.chunk.css"
+  },
+  {
+    "revision": "f399d68a0e6d0c84be0d",
+    "url": "/react-flashcards/static/js/2.86991bd1.chunk.js"
+  },
+  {
+    "revision": "c11a1a428ea8db8bdae6bbfe0a6f37c4",
+    "url": "/react-flashcards/index.html"
+  }
+];
